1d707c82b1160cf85de62899269a41e803931db1
bilaltiq <mtariq27@amherst.edu>
2024-04-28 19:21:38 -0400

    LEAFLET WORKS YAYAY

--- Projects/final-project/Final Project Shiny App.R
+++ Projects/final-project/Final Project Shiny App.R
@@ -1,16 +1,37 @@
 library(shiny)
 library(leaflet)
+library(dplyr)
+library(sf)
 
+# Reading the dataset with countries and their corresponding coordinates and cases
+COVIDCasesLatLong<- readRDS("Saved RDS Files/CountryCases.rds")
+
+####################################
 
 ui <- fluidPage(
-  titlePanel("Map of the World"),
+  titlePanel("COVID Cases in each Country"),
   leafletOutput("map")
 )
+
+
 server <- function(input, output, session) {
+  
+  
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(lng = -50, lat = 50, zoom = 1)
+      setView(lng = -50, lat = 50, zoom = 2)
+  })
+  
+  observe({
+    leafletProxy("map", data = COVIDCasesLatLong) %>%
+      addProviderTiles("CartoDB.Voyager") %>%
+      clearMarkers() %>%
+      addMarkers(~Longitude, ~Latitude, popup = ~paste("Country: ", Countries, "<br>Cases: ", Cases), 
+                  icon = leaflet::makeIcon(
+                  iconUrl = "Assets/icon.png",
+                  iconWidth = 38, iconHeight = 38
+                  ))
   })
   
   output$coords <- renderText({
