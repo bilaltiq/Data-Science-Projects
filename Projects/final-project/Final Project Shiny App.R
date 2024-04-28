c0b32beaa976b495db834af695de0018367371b4
bilaltiq <mtariq27@amherst.edu>
2024-04-28 17:17:45 -0400

    map

--- Projects/final-project/Final Project Shiny App.R
+++ Projects/final-project/Final Project Shiny App.R
@@ -1,10 +1,21 @@
 library(shiny)
+library(leaflet)
 
 
 ui <- fluidPage(
-  "Hello, world!"
+  titlePanel("Map of the World"),
+  leafletOutput("map")
 )
 server <- function(input, output, session) {
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      setView(lng = -50, lat = 50, zoom = 1)
+  })
+  
+  output$coords <- renderText({
+    paste("Latitude:", input$map_click$lat, "\nLongitude", input$map_click$lng)
+  })
 }
 
 
