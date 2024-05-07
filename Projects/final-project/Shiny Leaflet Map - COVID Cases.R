747d6900fb44a5a489bb55f1796add337c50552d
bilaltiq <mtariq27@amherst.edu>
2024-05-07 15:32:47 -0400

    Update Report

--- Projects/final-project/Shiny Leaflet Map - COVID Cases.R
+++ Projects/final-project/Shiny Leaflet Map - COVID Cases.R
@@ -15,22 +15,32 @@ colorPalette <- colorFactor(palette = c("blue", "red"), domain = COVIDCasesLatLo
 
 ui <- page_sidebar(
   title = "COVID Cases Per Country (2020)",
+  
+  #Sidebar Panel for Country Select (Drop down)
   sidebar = sidebar(
     "Sidebar",
     selectInput("country", "Select a country:", choices = unique(COVIDCasesLatLong$Countries))
   ),
+  
+  #Value Box that displays the number of COVID cases
   valueBoxOutput("valueBox"),
+  
+  #Leaflet Map Display
   card(leafletOutput("map")),
 )
 
 server <- function(input, output, session) {
   
+  
+  #Rendering the leaflet output
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       setView(lng = 0, lat = 0, zoom = 2)
   })
   
+  #Adding circular markers based on the number of COVID cases
+  #Circular markers react to clicks with a 'popup'
   observe({
     leafletProxy("map", data = COVIDCasesLatLong) %>%
       addProviderTiles("CartoDB.Voyager") %>%
@@ -39,6 +49,7 @@ server <- function(input, output, session) {
                        popup = ~paste("Country: ", Countries, "<br>Cases: ", Cases))
   })
   
+  #A value box output based on the input in the drop down 
   output$valueBox <- renderValueBox({
     selected_country <- input$country
     selected_cases <- COVIDCasesLatLong$Cases[COVIDCasesLatLong$Countries == selected_country]
